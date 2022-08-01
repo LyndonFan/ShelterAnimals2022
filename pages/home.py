65c8e5edcf55a8d1bda77e3f2c818b1efed4c923
LyndonFan <lyndon0808@gmail.com>
2022-08-01 01:19:54 +0100

    final prep

--- pages/home.py
+++ pages/home.py
@@ -17,9 +17,14 @@ layout = html.Div([
     html.H1('Shelter Animal Outcomes'),
     html.Hr(),
     html.Div([
-        html.P('This is a crude dashboard for the '),
-        html.A('Austin Animal Center Shelter Outcomes dataset',
-               href='https://www.kaggle.com/datasets/aaronschlegel/austin-animal-center-shelter-outcomes-and?select=aac_shelter_cat_outcome_eng.csv'),
-        html.P('. Its features are limited but hopefully there will be more pages!')
+        html.P(
+            'This is a crude dashboard for the Austin Animal Center Shelter Outcomes dataset.'),
+        html.P('Its features are limited but hopefully there will be more pages!'),
+        html.A('Link to Kaggle Dataset',
+               href='https://www.kaggle.com/datasets/aaronschlegel/austin-animal-center-shelter-outcomes-and?select=aac_shelter_cat_outcome_eng.csv',
+               ),
+        html.Br(),
+        html.A('Link to Plotly/Dash Contest Page',
+               href='https://community.plotly.com/t/summer-community-app-challenge/65099')
     ]),
 ])
